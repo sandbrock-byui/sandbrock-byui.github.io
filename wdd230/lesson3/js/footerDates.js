06ed880713e0c7142fc0c3603929b7a1569d9f34
Brandon Rock <sandbrock@byui.edu>
2020-10-02 17:49:27 -0600

    Added site plan

---
+++ wdd230/lesson3/js/footerDates.js
@@ -0,0 +1,7 @@
+var element = document.querySelector('.current-year');
+var currentDate = new Date();
+element.textContent = currentDate.getFullYear();
+
+
+var element = document.querySelector('.last-updated-time');
+element.textContent = document.lastModified;
