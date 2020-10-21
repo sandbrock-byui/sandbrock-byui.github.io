a4482d6d2759c1ed636c7cd59d0cccb5bc98fff4
Brandon Rock <sandbrock@byui.edu>
2020-10-20 19:56:58 -0600

    Added responsive hero

---
+++ wdd230/lesson6/js/footerDates.js
@@ -0,0 +1,10 @@
+const currentDateElement = document.getElementById('current-date');
+const currentDate = new Date();
+const dateFormatOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+};
+
+currentDateElement.textContent = currentDate.toLocaleDateString('en-US', dateFormatOptions);
