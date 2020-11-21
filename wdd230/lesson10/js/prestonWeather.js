b21a42b191d0254875f3a7384eaaacadb5dfae53
Brandon Rock <brandon@rockfam.org>
2020-11-21 15:22:02 -0700

    Changed max temp to current temp

--- wdd230/lesson10/js/prestonWeather.js
+++ wdd230/lesson10/js/prestonWeather.js
@@ -1,11 +1,4 @@
-var weekday = new Array(7);
-weekday[0] = "Sun";
-weekday[1] = "Mon";
-weekday[2] = "Tue";
-weekday[3] = "Wed";
-weekday[4] = "Thu";
-weekday[5] = "Fri";
-weekday[6] = "Sat";
+const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 // Calculate the day of week name
 const getDayOfWeek = (dateString) => {
@@ -20,22 +13,23 @@ fetch(weatherURL)
   .then(weather => {
     // Extract values
     let description = weather.weather[0].main;
-    let hightemp = weather.main.temp_max;
+    let temperature = weather.main.temp;
     let humidity = weather.main.humidity;
     let windspeed = weather.wind.speed;
 
     // Assign summary
     document.getElementById("summary-description").textContent = description;
-    document.getElementById("summary-high-temperature").textContent = hightemp + "℉";
+    document.getElementById("summary-temperature").textContent = temperature + "℉";
     document.getElementById("summary-humidity").textContent = humidity + "%";
     document.getElementById("summary-wind-speed").textContent = windspeed + "mph";
 
     // Calculate the wind chill
     let windchill = "N/A";
-    if (hightemp <= 50 && windspeed >= 3) {
-      windchill = 35.75 + 0.6215 * hightemp - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * hightemp * Math.pow(windspeed, 0.16);
-      document.getElementById("summary-wind-chill").textContent = Math.round(windchill) + "℉";
+    if (temperature <= 50 && windspeed >= 3) {
+      windchill = 35.75 + 0.6215 * temperature - 35.75 * Math.pow(windspeed, 0.16) + 0.4275 * temperature * Math.pow(windspeed, 0.16);
+      windchill = Math.round(windchill) + "℉";
     }
+    document.getElementById("summary-wind-chill").textContent = windchill;
   })
   .catch(error => {
     console.error("An error occurred fetching weather for Preston: " + error);
@@ -48,7 +42,7 @@ fetch(forecastURL)
   .then(forecast => {
     let currentChild = 1;
     for(const curForecast of forecast.list) {
-      if (!curForecast.dt_txt.includes("06:00:00")) continue;
+      if (!curForecast.dt_txt.includes("18:00:00")) continue;
 
       // Set the day of the week
       document.querySelector(`.five-day-forecast > article:nth-child(${currentChild}) h4`).textContent = getDayOfWeek(curForecast.dt_txt);
